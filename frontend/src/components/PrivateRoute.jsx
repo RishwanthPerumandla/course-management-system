7e3cafa0a0b413d548acdadfcbaf84e00427cbed
Rishwanth Perumandla <rishwanth100@gmail.com>
2024-04-29 18:31:21 -0400

    added refresh of pages

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext'; // Adjust this path as necessary
+import { useAuth } from '../context/AuthContext';
 
 function PrivateRoute({ children, role }) {
-    const { isAuthenticated,userRole } = useAuth();
+    const { isAuthenticated, userRole, loading } = useAuth();
     const location = useLocation();
 
+    if (loading) {
+        return <div>Loading...</div>; // Handle loading state appropriately
+    }
+
     if (!isAuthenticated) {
-        // Redirect to login
-        return <Navigate to="/" state={{ from: location }} replace />;
+        return <Navigate to="/login" state={{ from: location }} replace />;
     }
 
     if (role && userRole !== role) {
-        // Redirect to unauthorized or dashboard
         return <Navigate to="/unauthorized" replace />;
     }
 
     return children;
 }
 
-export default PrivateRoute; // Ensure this is a default export
+export default PrivateRoute;
